ec552af9febcde2a29cf3b9558e6292ff084714e
A.M <andrej2000aa@gmail.com>
2023-03-30 09:40:24 +0200

    prvi

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,29 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sass`,
+
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+
+
+
+
+  ],
 }
