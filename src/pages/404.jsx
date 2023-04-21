e6344bea2f62e899b370ac468fc39f7f68b44ccc
A.M <andrej2000aa@gmail.com>
2023-04-21 10:56:05 +0200

    error page ispravljen

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import '../assets/css/error.scss'
 
 
 const Error = () => {
 
     return (
-        <>
-            <h1>404</h1>
-            <h2>Page not found</h2>
-        </>
+        <Layout>
+            <div className="error-box">
+                <div className="error-text">
+                    <h1 >404</h1>
+                    <h2>Page not found</h2>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
